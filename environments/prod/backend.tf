193f28349c47d08dcb584311ec9ab653fedc32db
Abhiram <aram72772@gmail.com>
2026-02-17 00:06:45 +0530

    Delete environments directory

--- environments/prod/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "<terraform-state-rg>"
-    storage_account_name = "<terraform-state-storage-account>"
-    container_name       = "tfstate"
-    key                  = "prod/terraform.tfstate"
-  }
-}
